b37d6dff44b90908341f9bf613f6541e55aad91d
Vlad <0vsyannikovv24@gmail.com>
2020-06-30 19:37:59 +0700

    Добавлено сохранение состояния

--- app/src/main/java/com/example/draganddraw/Box.java
+++ app/src/main/java/com/example/draganddraw/Box.java
@@ -1,8 +1,10 @@
 package com.example.draganddraw;
 
 import android.graphics.PointF;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Box {
+public class Box implements Parcelable {
     private PointF mOrigin;
     private PointF mCurrent;
 
@@ -22,4 +24,33 @@ public class Box {
     public PointF getOrigin() {
         return mOrigin;
     }
+
+    protected Box(Parcel in) {
+        mOrigin = (PointF) in.readValue(PointF.class.getClassLoader());
+        mCurrent = (PointF) in.readValue(PointF.class.getClassLoader());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(mOrigin);
+        dest.writeValue(mCurrent);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Box> CREATOR = new Parcelable.Creator<Box>() {
+        @Override
+        public Box createFromParcel(Parcel in) {
+            return new Box(in);
+        }
+
+        @Override
+        public Box[] newArray(int size) {
+            return new Box[size];
+        }
+    };
 }
--- app/src/main/java/com/example/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/example/draganddraw/BoxDrawingView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -15,6 +17,8 @@ import java.util.List;
 public class BoxDrawingView extends View {
 
     private static final String TAG = "BoxDrawingView";
+    private static final String PARENTVIEW_SAVED_STATE = "ParentView";
+    private static final String CHILDVIEW_SAVED_STATE = "ChildView";
 
     private Box mCurrentBox;
     private List<Box> mBoxen = new ArrayList<>();
@@ -78,5 +82,21 @@ public class BoxDrawingView extends View {
 
         Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
         return true;
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Parcelable viewState = super.onSaveInstanceState();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(PARENTVIEW_SAVED_STATE, viewState);
+        bundle.putParcelableArrayList(CHILDVIEW_SAVED_STATE, (ArrayList<Box>) mBoxen);
+        return bundle;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        Bundle bundle = (Bundle) state;
+        super.onRestoreInstanceState(bundle.getParcelable(PARENTVIEW_SAVED_STATE));
+        mBoxen = bundle.getParcelableArrayList(CHILDVIEW_SAVED_STATE);
     }
 }
